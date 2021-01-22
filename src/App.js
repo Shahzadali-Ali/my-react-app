69d6666d3c5b9bda57da43991a4e5c4028ea542f
Shahzad <shahzadali.infonic@gmail.com>
2021-01-22 21:48:54 +0530

    your message

--- src/App.js
+++ src/App.js
@@ -6,15 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome To React
-        </a>
+        <p>Welcome To React</p>        
       </header>
     </div>
   );
